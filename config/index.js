11e743205a1b12f2809a3608aedc28b3b0ac90cf
DarellYuhu <s2200064@student.unklab.id>
2023-06-19 10:33:07 +0800

    fix: change firebase configuration for production

--- config/index.js
+++ config/index.js
@@ -1,13 +1,16 @@
-const { initializeApp } = require('firebase/app');
-const { getAuth, createUserWithEmailAndPassword, otp } = require("firebase/auth");
+const { initializeApp } = require("firebase/app");
+const {
+  getAuth,
+  createUserWithEmailAndPassword,
+  otp,
+} = require("firebase/auth");
 const firebaseConfig = {
-    apiKey: "AIzaSyASGhrzhbiJjx0anndtCsnLLO_6XYMPOAE",
-    authDomain: "aime-sulut.firebaseapp.com",
-    projectId: "aime-sulut",
-    storageBucket: "aime-sulut.appspot.com",
-    messagingSenderId: "17458371709",
-    appId: "1:17458371709:web:8522456174fd2950586514",
-    measurementId: "G-ZQ1L5H3E29"
+  apiKey: "AIzaSyBrigAza54IO6NxMKJ1qPa3sInB8xkGkX0",
+  authDomain: "aime-390301.firebaseapp.com",
+  projectId: "aime-390301",
+  storageBucket: "aime-390301.appspot.com",
+  messagingSenderId: "65604286298",
+  appId: "1:65604286298:web:ad9ffd380c5c1d86fc2563",
 };
 
 const firebase = initializeApp(firebaseConfig);
